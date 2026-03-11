1a114c52f26b7a62af8ea0e53b57858eb0c6f828
Austin Reed <acreed1998@gmail.com>
2026-03-10 19:10:09 -0500

    chore: remove unused imports

--- src/mpv_playlist_maker/main.py
+++ src/mpv_playlist_maker/main.py
@@ -1,23 +1,12 @@
 from utils import (
-    determineDirectoryToUse,
     determineVideosToPlay,
-    getRuntimeJSON,
-    getVideoFiles,
-    determineSortOrder,
-    sortVideoFiles,
     playVideos,
 )
 
 
 def main():
     videoList, index = determineVideosToPlay()
-    # playVideos(videoList, index)
-    # directory = determineDirectoryToUse()
-    # files = getVideoFiles(directory)
-    # sortBy, sortOrder = determineSortOrder()
-    # sortedFiles = sortVideoFiles(files, sortBy, sortOrder)
-    # playVideos(sortedFiles)
-    print(videoList)
+    playVideos(videoList, index)
 
 
 if __name__ == "__main__":
